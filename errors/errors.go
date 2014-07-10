bfe8c0e51de3978aa52f4864a7057c6e8b6f4a05
Denis Zubkov <deze333@yahoo.com>
2014-07-10 16:53:48 -0700

    ui error

--- errors/errors.go
+++ errors/errors.go
@@ -1,7 +1,7 @@
 package errors
 
 import (
-    "strings"
+	"strings"
 )
 
 //------------------------------------------------------------
@@ -9,47 +9,47 @@ import (
 //------------------------------------------------------------
 
 const (
-    ERR_NOT_FOUND = "NOT_FOUND"  // requested URL not found
-    ERR_NOT_AUTHD = "NOT_AUTHD"  // user not authenticated for this request
-    ERR_APP       = "APP_ERR"    // error on app side (can't open file, etc.)
-    ERR_REQ       = "REQ_ERR"    // erroneous request (wrong format, etc.)
+	ERR_NOT_FOUND = "NOT_FOUND" // requested URL not found
+	ERR_NOT_AUTHD = "NOT_AUTHD" // user not authenticated for this request
+	ERR_APP       = "APP_ERR"   // error on app side (can't open file, etc.)
+	ERR_REQ       = "REQ_ERR"   // erroneous request (wrong format, etc.)
 )
 
 // Response error
 type ResError struct {
-    Code string  `json:"code,omitempty"`
-    Err  string  `json:"err,omitempty"`
-    Msg  string  `json:"msg,omitempty"`
+	Code string `json:"code,omitempty"`
+	Err  string `json:"err,omitempty"`
+	Msg  string `json:"msg,omitempty"`
 }
 
 // Satisfies error interface.
 func (e *ResError) Error() string {
-    return e.Code + ": " + e.Msg + ": " + e.Err
+	return e.Code + ": " + e.Msg + ": " + e.Err
 }
 
 // Parses error and creates new ResError.
 func New(err error) *ResError {
 
-    s := err.Error()
-    if strings.HasPrefix(s, "ERR:") {
-        // match length of "ERR:" prefix
-        return &ResError{ERR_REQ, "Request error", s[4:]} 
-    } else {
-        return &ResError{ERR_APP, s, "Application error"}
-    }
+	s := err.Error()
+	if strings.HasPrefix(s, "UI_ERR:") {
+		// match length of "UI_ERR:" prefix
+		return &ResError{ERR_REQ, "Request error", s[7:]}
+	} else {
+		return &ResError{ERR_APP, s, "Application error"}
+	}
 }
 
 // New appliation error.
 func New_AppErr(err error, msg string) *ResError {
-    return &ResError{ERR_APP, err.Error(), msg}
+	return &ResError{ERR_APP, err.Error(), msg}
 }
 
 // URL not found.
 func New_NotFound(url, msg string) *ResError {
-    return &ResError{ERR_NOT_FOUND, url, msg}
+	return &ResError{ERR_NOT_FOUND, url, msg}
 }
 
 // User not authenticated.
 func New_NotAuthd() *ResError {
-    return &ResError{ERR_NOT_AUTHD, "User not authenticated", "Please login to proceed"}
+	return &ResError{ERR_NOT_AUTHD, "User not authenticated", "Please login to proceed"}
 }
